a857606f0303f9da6a95e41ca276b97a58957435
Egin Pardewan <eginpardewan@gmail.com>
2024-10-22 18:45:05 +0700

    setup server express dengan body-parser dan routing Student

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import Student from './routes/Student.js';  // Pastikan huruf kecil sesuai dengan nama file
+
+const app = express();
+const port = 8000;
+
+// Middleware untuk parsing JSON
+app.use(bodyParser.json());
+
+// Routing untuk /Student
+app.use("/Student", Student);
+
+// Routing untuk root '/'
+app.get("/", (req, res) => {
+    console.log(['GET ROUTE']);
+    res.send("Selamat Pagi");
+});
+
+// Jalankan server di port 8000
+app.listen(port, () => console.log(`Server berjalan di port: http://localhost:${port}`));
